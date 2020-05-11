e74cb2958d99a407d4c2561f0b83b97df69868b2
Steffan Lloyd <steffan@sportlog.ca>
2020-05-11 16:57:06 -0400

    updates

--- +PSDM/config.m
+++ +PSDM/config.m
@@ -13,7 +13,7 @@ function c = config()
     %   - PSDM.generateYp
     %   - PSDM.inverseDynamics
     %   - PSDM.forwardDynamics
-    c.use_mex_basic = true;
+    c.use_mex_basic = false;
     
     % If set to true, after each step of the algorithm, extra computations
     % will be done to double-check that the derived model is still exactly
