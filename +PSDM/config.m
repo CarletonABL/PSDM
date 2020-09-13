4084320790c4fa5d509c6ccef7235b92ac6ea98a
Steffan Lloyd <steffan.lloyd@carleton.ca>
2020-09-13 19:56:27 -0400

    Update config.m
    Update to config file

--- +PSDM/config.m
+++ +PSDM/config.m
@@ -14,4 +14,9 @@ function c = config()
     % accurate. Recommended to keep this as true!
     c.do_reprojection_tests = coder.target('matlab');
     
+    % Whether or not to use parallel computing to speed up the algorithm
+    % derivation. Note, setting this to true will result in an error, if
+    % the Parallel Computing Toolbox isn't installed.
+    c.use_par = true;
+    
 end
