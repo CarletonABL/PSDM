dc4a8afb3ad8607f771d2c76ecceac259e7a8ccb
Steffan Lloyd <steffan@sportlog.ca>
2020-09-19 19:49:26 -0400

    updates

--- +PSDM/config.m
+++ +PSDM/config.m
@@ -7,11 +7,16 @@ function c = config()
     %   - PSDM.generateYp
     %   - PSDM.inverseDynamics
     %   - PSDM.forwardDynamics
-    c.use_mex = false;
+    c.use_mex = true;
     
     % If set to true, after each step of the algorithm, extra computations
     % will be done to double-check that the derived model is still exactly
     % accurate. Recommended to keep this as true!
     c.do_reprojection_tests = coder.target('matlab');
+    
+    % Whether or not to use parallel computing to speed up the algorithm
+    % derivation. Note, setting this to true will result in an error, if
+    % the Parallel Computing Toolbox isn't installed.
+    c.use_par = true;
     
 end
